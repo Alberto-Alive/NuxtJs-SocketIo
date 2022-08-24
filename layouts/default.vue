ce9005841e8997d55e8665c299f204352ee7ad28
Alive <albertopopescu@outlook.com>
2022-08-24 05:54:39 +0100

    get rid of drawer and add them buttons on the top bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,5 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
       fixed
@@ -50,6 +26,22 @@
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
+     
+      <v-btn
+        router
+        exact
+        to="/"
+      >
+        <v-icon>mdi-chat</v-icon>
+      </v-btn>
+      <v-btn
+         router
+        exact
+        to="/inspire"
+      >
+        <v-icon>mdi-account-multiple-plus</v-icon>
+      </v-btn>
+      <v-spacer/>
       <v-btn
         icon
         @click.stop="rightDrawer = !rightDrawer"
@@ -112,6 +104,11 @@ export default {
       right: true,
       rightDrawer: false,
       title: 'Vuetify.js'
+    }
+  },
+  meethods:{
+    changeToInspire(){
+      this.$router.push('/inspire')
     }
   }
 }
